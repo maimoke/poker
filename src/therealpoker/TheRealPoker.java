ced2c8513517a12efffa22478d91701410375e72
Moke <Moke@DESKTOP-GCM80HM>
2021-04-11 23:31:18 +0700

    gu perd eng

---
+++ src/therealpoker/TheRealPoker.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package therealpoker;
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+/**
+ *
+ * @author WanwaiStdio
+ */
+public class TheRealPoker extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+        Pane root = loader.load();
+
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Poker");
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+           launch(args);
+    }
+    
+}
