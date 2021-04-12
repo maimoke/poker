8c2638a849b02c09b8c2d91ef4ed1cdecb6f604e
THANAPAT SITTIPORNCHAISAKUL <oatkung.ts@gmail.com>
2021-04-12 21:27:13 +0700

    Deck

---
+++ src/therealpoker/Deck.java
@@ -0,0 +1,33 @@
+package therealpoker;
+import java.util.Random;
+public class Deck {
+    Card[] cardIndeck = new Card[52];    
+    int cardAmount;
+    
+    Card draw(){       
+        Card[] temp = cardIndeck;        
+        cardIndeck = new Card[cardIndeck.length-1];
+        for (int i = 0; i < cardIndeck.length; i++) {
+            cardIndeck[i] = temp[i+1];
+        }
+        return temp[cardAmount++]; //first out
+    }
+    
+    void shuffle(){
+         Random rand = new Random();                   
+         for (int i = 0; i < cardIndeck.length; i++) {
+         int r = i + rand.nextInt(52 - i);
+         Card temp = cardIndeck[r];
+             cardIndeck[r] = cardIndeck[i];
+             cardIndeck[i] = temp;
+        }
+         
+    }
+    void discard(){
+        Card[] temp = cardIndeck;        
+        cardIndeck = new Card[cardIndeck.length-1];
+        for (int i = 0; i < cardIndeck.length; i++) {
+            cardIndeck[i] = temp[i+1];
+        }
+    }
+}
