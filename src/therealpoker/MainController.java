d9fc318e9f34ff972691dcd8ccf70c898045c65d
Moke <Moke@DESKTOP-GCM80HM>
2021-04-14 23:51:09 +0700

    ai here bingo por mung tai ai sus ai na hee

--- src/therealpoker/MainController.java
+++ src/therealpoker/MainController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -39,6 +40,24 @@ public class MainController implements Initializable {
     private Button btn_call;
     @FXML
     private AnchorPane Game;
+    @FXML
+    private ImageView player1;
+    @FXML
+    private ImageView player2;
+    @FXML
+    private ImageView player3;
+    @FXML
+    private ImageView player4;
+    @FXML
+    private ImageView player5;
+    @FXML
+    private ImageView player6;
+    @FXML
+    private ImageView player7;
+    @FXML
+    private ImageView player8;
+    @FXML
+    private ImageView dealer;
 
     /**
      * Initializes the controller class.
@@ -61,6 +80,17 @@ public class MainController implements Initializable {
         fill_raise.setVisible(false);
         btn_check.setVisible(false);
         btn_call.setVisible(false);
+        
+        //player
+        player1.setVisible(false);
+        player2.setVisible(false);
+        player3.setVisible(false);
+        player4.setVisible(false);
+        player5.setVisible(false);
+        player6.setVisible(false);
+        player7.setVisible(false);
+        player8.setVisible(false);
+        dealer.setVisible(false);
     }
 
     @FXML
